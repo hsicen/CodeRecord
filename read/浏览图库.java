cf46d89e7a454e48d02174d1c36455827ca4561e
黄思程 <huangsicheng@gouuse.cn>
2018-04-13 13:45:32 +0800

    资料整理，项目整合

---
+++ read/浏览图库.java
@@ -0,0 +1,169 @@
+package com.android.poetic.helloandroid;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
+    private static final int PICK_PICTURE = 0x01;
+    private static final int CHOOSE_PHOTO = 0x02;
+
+    private Button mbtnSelect;
+    private ImageView mivImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mbtnSelect = ((Button) findViewById(R.id.btn_select));
+        mivImg = ((ImageView) findViewById(R.id.iv_img));
+
+        mbtnSelect.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PICK_PICTURE);
+        else
+            openAlbum();
+    }
+
+    /**
+     * 打开相册
+     */
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PICK_PICTURE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    openAlbum();
+                else
+                    ToastUtils.showShort(MainActivity.this, "You denied the permission");
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19)
+                        handleImageOnKitKat(data);
+                    else
+                        handleImageBeforeKitKat(data);
+                }
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    /**
+     * 在api 19之前处理
+     *
+     * @param data
+     */
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imgPath = getImagePath(uri, null);
+        displayImg(imgPath);
+    }
+
+    /**
+     * 在 api 19及以上的处理
+     *
+     * @param data
+     */
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imgPath = null;
+        Uri uri = data.getData();
+
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            //如果是Document类型的uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1]; //解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imgPath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse
+                        ("content://downloads/public_downloads"), Long.valueOf(docId));
+                imgPath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            //如果是content类型的uri，则使用普通方式处理
+            imgPath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            imgPath = uri.getPath();
+        }
+
+        displayImg(imgPath);  //根据图片路径展示图片
+    }
+
+    /**
+     * 根据path，展示图片
+     *
+     * @param imgPath
+     */
+    private void displayImg(String imgPath) {
+        if (imgPath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imgPath);
+            mivImg.setImageBitmap(bitmap);
+        } else
+            ToastUtils.showShort(this, "Filed to get image");
+    }
+
+    /**
+     * 根据uri获取图片的path路径
+     *
+     * @param uri
+     * @param selection
+     * @return
+     */
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        //根据uri和selection来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst())
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            cursor.close();
+        }
+
+        return path;
+    }
+}
