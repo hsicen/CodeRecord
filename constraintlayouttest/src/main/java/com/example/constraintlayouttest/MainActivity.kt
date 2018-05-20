89ca96f8fcb501744808cfd2a0cde9f38690b92f
NightYoung <codinghuang@163.com>
2018-05-20 09:19:14 +0800

    约束布局的使用

---
+++ constraintlayouttest/src/main/java/com/example/constraintlayouttest/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.constraintlayouttest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Author ：NightYoung  2018/5/15 16:08
+ * Email    ：codinghuang@163.com
+ * Func     ：
+ * Desc     ：The use of ConstraintLayout
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
