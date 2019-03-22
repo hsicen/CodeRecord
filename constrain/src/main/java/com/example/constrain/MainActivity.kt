77dd77601e8648d544eb87c757611781f483e95e
Night <codinghuang@163.com>
2019-03-22 16:11:41 +0800

    约束布局属性尝试

--- constrain/src/main/java/com/example/constrain/MainActivity.kt
+++ constrain/src/main/java/com/example/constrain/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
  * <p>描述：约束布局
  *
  * 属性1(RelativeLayout)： 控制一个控件在另一个控件的左边还是右边，上边还是下边
- * layout_constraintXxx_toXxxOf
+ * layout_constraintXxx_toXxxOf(我的左/右/上/下 和你的左/右/上/下 对齐)
  * layout_constraintBaseline_toBaselineOf
  *
  * 属性2：设置控件显示宽高比例
@@ -33,9 +33,25 @@ import androidx.appcompat.app.AppCompatActivity
  * spread_inside + 宽度非0：起始平分
  * packed + 宽度非0：集中居中
  *
- * 属性5：设置bias(拉力大小)
+ * 属性5：设置bias(控制两边拉力大小)
+ * layout_constraintHorizontal_bias="0.9"(左边0.1，右边0.9)
+ * layout_constraintVertical_bias="0.9"(上边0.1，下边0.9)
  *
+ * 属性6：设置Guideline(布局辅助线)
+ * orientation="horizontal" (设置水平或垂直的辅助线)
+ * 指定辅助线的位置
+ * layout_constraintGuide_begin="50dp"
+ * layout_constraintGuide_end="50dp"
+ * layout_constraintGuide_percent="50dp"
  *
+ * 属性7：圆形定位
+ * app:layout_constraintCircle="@id/tv_float"
+ * app:layout_constraintCircleAngle="145"
+ * app:layout_constraintCircleRadius="18dp"
+ *
+ * 属性8：Barrier(整体屏障)
+ *
+ * 属性9：Group(组，控制组内控件整体的状态)
  *
  */
 class MainActivity : AppCompatActivity() {
